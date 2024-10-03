f9e43543b8585e89feabaa0ee6b9ea3a19f5a311
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-10-03 02:09:55 -0500

    Fin del proyecto

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,10 +10,7 @@ export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 // FORMAT DATE TIME
-export const formatDateTime = (
-  dateString: Date | string,
-  timeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone,
-) => {
+export const formatDateTime = (dateString: Date | string, timeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone) => {
   const dateTimeOptions: Intl.DateTimeFormatOptions = {
     // weekday: "short", // abbreviated weekday name (e.g., 'Mon')
     month: "short", // abbreviated month name (e.g., 'Oct')
@@ -49,22 +46,22 @@ export const formatDateTime = (
 
   const formattedDateTime: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateTimeOptions,
+    dateTimeOptions
   );
 
   const formattedDateDay: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateDayOptions,
+    dateDayOptions
   );
 
   const formattedDate: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateOptions,
+    dateOptions
   );
 
   const formattedTime: string = new Date(dateString).toLocaleString(
     "en-US",
-    timeOptions,
+    timeOptions
   );
 
   return {
